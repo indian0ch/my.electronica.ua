7cdc718d5fa4fc2e21c38b41c1d8e511b4c928d3
indian0ch <fesiukandrey@gmail.com>
2023-07-11 17:57:42 +0300

    Added redux-persist

--- src/components/storage/store.js
+++ src/components/storage/store.js
@@ -1,9 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import colorChooseReducer from "./colorChoose-slice";
 import cartReducer from "./cart-slice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 
-const store = configureStore({
-  reducer: { colorChoosen: colorChooseReducer, cartFunctional:cartReducer },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducers = combineReducers({
+  colorChoosen: colorChooseReducer,
+  cartFunctional: cartReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
 export default store;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./components/storage/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, {persistor} from "./components/storage/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
